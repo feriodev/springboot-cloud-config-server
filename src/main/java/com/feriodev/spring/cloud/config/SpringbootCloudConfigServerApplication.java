6e6b494415827ad2a4347b4bbb5b44fde0fc00de
Daniel-Cuadros_pacifico <daniel.cuadros@teamsoft.com.pe>
2024-05-09 10:51:21 -0500

    Initial commit

---
+++ src/main/java/com/feriodev/spring/cloud/config/SpringbootCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.feriodev.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringbootCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feriodev/spring/cloud/config/SpringbootCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feriodev.spring.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
